35d6170bd910ad39da7eef0f163746c981154cb4
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2024-02-13 14:36:47 +0100

    Initial commit

---
+++ src/main/java/com/randian368/jottodo/JottodoApplication.java
@@ -0,0 +1,23 @@
+package com.randian368.jottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JottodoApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		SpringApplication.run(JottodoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/randian368/jottodo/JottodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randian368.jottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JottodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
